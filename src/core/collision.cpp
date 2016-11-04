b2c4e572542c4b5a987af531575f1eae8b73fba5
Sebastian <info@bindgen.net>
2016-11-04 20:10:19 +0100

    Fixed Rudolf's sketch

--- src/core/collision.cpp
+++ src/core/collision.cpp
@@ -55,7 +55,7 @@ static int number_of_collisions, total_collisions;
 /// Parameters for collision detection
 Collision_parameters collision_params = { 0, };
 
-int collision_detection_set_params(int mode, double d, int bond_centers, int bond_vs,int t,int d2, int tg, int tv, int ta, int bond_three_particles, int angle_resolution)
+int collision_detection_set_params(int mode, double d, int bond_centers, int bond_vs,int t,int d2, int tg, int tv, int ta, int bond_three_particles, int angle_resolution, double rel_vs_placement)
 {
   // The collision modes involving virutal istes also requires the creation of a bond between the colliding 
   // particles, hence, we turn that on.
@@ -679,10 +679,9 @@ void handle_collisions ()
 	{
    // Recalculate positiosn of vs for those cases, where two vs
    // are not supposed to be calculated at the same location
-	 double vs_pos1[3],vs_pos2[3],vec21;
-   Particle* p1=local_particles[collision_queue[i].pp1],p2=local_particles[collision_queue[i].pp2];
+	 double vs_pos1[3],vs_pos2[3],vec21[3];
+   Particle* p1=local_particles[collision_queue[i].pp1],*p2=local_particles[collision_queue[i].pp2];
    get_mi_vector(vec21,p1->r.p,p2->r.p);
-   double d=sqrt(SQRLEN(vec21));
    for (int k=0;k<3;k++){
      vs_pos1[k]=p1->r.p[k] -collision_params.rel_vs_placement *vec21[k];
      vs_pos2[k]=p1->r.p[k] -(1-collision_params.rel_vs_placement) *vec21[k];
@@ -691,7 +690,7 @@ void handle_collisions ()
    place_vs_and_relate_to_particle(vs_pos2,collision_queue[i].pp2);
 	}
   else
-  if (collision_params.mode & COLLISION_MODE_GLUE_TO_SURFACE ) {
+  if (collision_params.mode & COLLISION_MODE_GLUE_TO_SURF ) {
    place_vs_and_relate_to_particle(collision_queue[i].point_of_collision,collision_queue[i].pp2);
   }
   
