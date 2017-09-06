8544ab14aeaf5f6d95b819d0968521f2a3558f20
schobecn <schobecn@ipvs.uni-stuttgart.de>
2017-09-06 16:32:04 +0200

    Script interface component for CollisionDetection

--- src/script_interface/collision_detection/initialize.cpp
+++ src/script_interface/collision_detection/initialize.cpp
@@ -1,57 +1,34 @@
 /*
   Copyright (C) 2015,2016 The ESPResSo project
-
   This file is part of ESPResSo.
-
   ESPResSo is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-
   ESPResSo is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-
 #include "config.hpp"
+
 #include "initialize.hpp"
-#include "constraints/initialize.hpp"
-#include "shapes/initialize.hpp"
-#ifdef H5MD
-#include "h5md/initialize.hpp"
-#endif
-#include "observables/initialize.hpp" 
-#include "correlators/initialize.hpp" 
-#include "lbboundaries/initialize.hpp"
-#include "collision_detection/initialize.hpp"
+#include "ScriptInterface.hpp"
 
-#include "ParallelScriptInterface.hpp"
-#include "VariantTester.hpp"
+#include "CollisionDetection.hpp"
 
-#include "core/communication.hpp"
 
 namespace ScriptInterface {
+  namespace CollisionDetection {
 
-void initialize() {
-  ParallelScriptInterface::initialize(Communication::mpiCallbacks());
 
-  Shapes::initialize();
-  Constraints::initialize();
-#ifdef H5MD
-  Writer::initialize();
+    void initialize() {
+#ifdef COLLISION_DETECTION
+      ScriptInterface::register_new<ScriptInterface::CollisionDetection::CollisionDetection>(
+											     "CollisionDetection::CollisionDetection");
 #endif
-  Observables::initialize();
-  Correlators::initialize();
-  LBBoundaries::initialize();
-
-  CollisionDetection::initialize();
-  
-  ScriptInterface::register_new<Testing::VariantTester>("Testing::VariantTester");
-}
-
+    }
+  } /* namespace CollisionDetection */
 } /* namespace ScriptInterface */
